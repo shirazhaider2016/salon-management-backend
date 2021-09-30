b1376d6525e4fc84c1078297ac8809cbb898d706
shiraz <muhammad.shiraz@arkhitech.com>
2021-09-30 20:01:21 +0500

    admin lte added and devise added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'jquery/dist/jquery.slim'
+import 'popper.js/dist/esm/popper'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "stylesheets/application"
+require('admin-lte');
+import "@fortawesome/fontawesome-free/js/all";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+});
